993eff622d5b404ee68c90888dc7e263aceb5f20
maxencce <maxencemichel37@gmail.com>
2024-09-20 09:30:20 +0200

    Fin question 6

--- interaction.js
+++ interaction.js
@@ -2,9 +2,18 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+    this.canvas = canvas;
+    this.interactor = interactor;
+    this.posInit = {x:0, y:0};
+    this.posFin = {x:0, y:0};
+    this.pressed = false;
 
 	// Developper les 3 fonctions gérant les événements
+    canvas.addEventListener('mousedown', this.mousePressed.bind(this), false); // Associer les fonctions précédentes aux évènements du canvas.
+    canvas.addEventListener('mousemove', this.mouseMoved.bind(this), false); // Associer les fonctions précédentes aux évènements du canvas.
+    canvas.addEventListener('mouseup', this.mouseReleased.bind(this), false);
+
+
 
 	// Associer les fonctions précédentes aux évènements du canvas.
 };
@@ -19,5 +28,23 @@ function getMousePosition(canvas, evt) {
   };
 };
 
+DnD.prototype.mousePressed = function(evt) {
+    this.posInit = getMousePosition(this.canvas, evt);
+    this.pressed = true;
+    console.log("Mouse pressed " + this.posInit.x + " " + this.posInit.y);
+}
+
+DnD.prototype.mouseMoved = function(evt) {
+    if (this.pressed) {
+        this.posFin = getMousePosition(this.canvas, evt);
+        console.log("Mouse moved " + this.posFin.x + " " + this.posFin.y);
+    }
+}
+
+DnD.prototype.mouseReleased = function(evt) {
+    this.posFin = getMousePosition(this.canvas, evt);
+    this.pressed = false;
+    console.log("Mouse released " + this.posFin.x + " " + this.posFin.y);
+}
 
 
--- model.js
+++ model.js
@@ -1,3 +1,27 @@
+function Drawing() {
+    this.shapes = new Array();
+}
 
+function Shape(lineWidth, color) {
+    this.color = color;
+    this.lineWidth = lineWidth;
+}
+
+function Rectangle(x, y, width, height, lineWidth, color) {
+    Shape.call(this, color, lineWidth);
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+}
+
+function Line(x1, y1, x2, y2, lineWidth, color) {
+    Shape.call(this, color, lineWidth);
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+
+}
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
